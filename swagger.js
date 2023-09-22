ee623b1202fa08fe3a69f66e5b4e84e21a9dc38f
Shashwat2104 <shashwatmahender2104@gmail.com>
2023-09-22 08:16:03 +0530

    Swagger_Updated

--- swagger.js
+++ swagger.js
@@ -1,15 +1,16 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   swaggerDefinition: {
-    openapi: '3.0.0', // Use the appropriate version (3.0.0 for Swagger 3.0)
+    openapi: "3.0.0", // Use the appropriate version (3.0.0 for Swagger 3.0)
     info: {
-      title: 'Ecommerce-API with Routes',
-      version: '1.0.0',
-      description: 'Ecommerce-API',
+      title: "Ecommerce-API",
+      version: "1.0.0",
+      description:
+        "API for managing an e-commerce website, including user registration, product management, shopping cart, and order placement.",
     },
   },
-  apis: ['./routes/*.js'], // Update with the path to your route files
+  apis: ["./routes/*.js"], // Update with the path to your route files
 };
 
 const specs = swaggerJsdoc(options);
