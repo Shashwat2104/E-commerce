8c7299da2fffe18847cf50ad6f09a7d959bcc70b
Shashwat2104 <shashwatmahender2104@gmail.com>
2023-09-22 15:52:54 +0530

    Routes_Completed

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const UserModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = UserModel;
